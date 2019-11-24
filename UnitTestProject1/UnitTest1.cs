36e2fb52497835b7de3fc3ca8b1b79814494bc0e
rajasekhar229 <rajasekhar229@gmail.com>
2019-11-23 18:53:21 -0600

    unittest updated

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using Newtonsoft.Json.Linq;
+using NSubstitute;
+using Retail.Controllers;
+using Retail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAllProducts_ReturnProductsById()
+        {
+            // Arrange
+            var productservice = Substitute.For<IProductService>();
+            int _id = 13860428;
+            var productlist = productslist().Where(a => a.Id == _id).FirstOrDefault();
+            productservice.GetById(_id).Returns(productlist);
+            productservice.GetProductName().Returns(productlist.ProductName);
+            var productcontroller = new ProductsController(productservice)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+
+            // Act
+            var productswew = productcontroller.Get(_id).Content.ReadAsStringAsync().Result;
+            JObject jObject = JObject.Parse(productswew);
+
+
+            Products product;
+            var products = productcontroller.Get(_id);
+            Assert.IsTrue(products.TryGetContentValue<Products>(out product));
+
+            // Assert
+            Assert.AreEqual(productlist.ProductName, product.ProductName);
+            Assert.AreEqual(_id, jObject["Id"]);
+        }
+
+        [TestMethod]
+        public void GetAllProducts_PutProductsPrice()
+        {
+            // Arrange
+            var productservice = Substitute.For<IProductService>();
+            int _id = 13860429;
+            var productlist = productslist().Where(a => a.Id == _id).FirstOrDefault();
+            productservice.GetById(_id).Returns(productlist);
+
+            var productcontroller = new ProductsController(productservice)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+
+            productlist.CurrentPrice["value"] = "50";
+            productservice.Update(productlist).Returns(productlist);
+
+            // Act
+            var productswew = productcontroller.Put(productlist).Content.ReadAsStringAsync().Result;
+            JObject jObject = JObject.Parse(productswew);
+
+            // Assert 
+            Assert.AreEqual(50, Convert.ToInt32(jObject["CurrentPrice"]["value"].ToString()));
+        }
+
+
+        private List<Products> productslist()
+        {
+            List<Products> prd = new List<Products>()
+            {
+                new Products(){ Id=13860428,ProductName="The Big Lebowski (Blu-ray) ",
+                                CurrentPrice =new Dictionary<string, string>
+                                { { "value", "13.49" },{ "currency_code", "USD" } } },
+                new Products(){ Id=13860429,ProductName="",
+                                CurrentPrice =new Dictionary<string, string>
+                                { { "value", "14.49" },{ "currency_code", "USD" } } },
+                new Products(){ Id=13860430,ProductName="LCD TV",
+                                CurrentPrice =new Dictionary<string, string>
+                                { { "value", "15.49" },{ "currency_code", "USD" } } }
+
+            };
+            return prd;
+        }
+    }
+}
